32402e7efd67f76ee2529160006e5e96d51e6bfe
Munashif Muhsin <munashif.muhsin@gmail.com>
2024-01-15 13:13:33 +0400

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+// Get Environment and set api urls.
+const endpoint = String.fromEnvironment(
+  'env',
+  defaultValue: 'prod',
+);
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(child: Text("Env: $endpoint")),
+    );
+  }
+}
